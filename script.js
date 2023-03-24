16b1d2b6fb602c4bca6f583c26a9c34a238bc97c
Rida Berkaoui <ridaberkaoui1997@gmail.com>
2023-03-24 07:53:24 -0400

    Configure book constructor Configure function that adds new rows to the table Configure function that displays books (title, author and pages only for now) Closing book modal now clears book form

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ const addBtn = document.querySelector(".book-button--add");
 const deleteAllBtn = document.querySelector(".book-button--delete");
 const bookCloseBtn = document.querySelector(".book-button--close");
 const deleteCloseBtn = document.querySelector(".delete-close-button");
+const doneBtn = document.querySelector(".submit");
+const table = document.querySelector(".table");
+
+// Book Variables
+const bookTitle = document.querySelector(".book-title");
+const bookAuthor = document.querySelector(".book-author");
+const bookPages = document.querySelector(".book-pages");
+const bookRead = document.querySelector(".book-read");
+const checkmark = document.querySelector(".check");
+
+// Table Variables
+
+let tr;
+let tdTitle;
+let tdAuthor;
+let tdPages;
+let tdRead;
+let tdRemove;
+
+// ***** MODALS ***** //
 
 // Events to open/close new book modal
 addBtn.addEventListener("click", function () {
@@ -39,6 +59,9 @@ function openBookModal() {
 }
 
 function closeBookModal() {
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  bookPages.value = "";
   bookModal.classList.remove("active");
   overlay.classList.remove("active");
 }
@@ -54,3 +77,83 @@ function closeDeleteModal() {
   deleteModal.classList.remove("active");
   overlay.classList.remove("active");
 }
+
+//////////////////////////////////////////////
+
+// Library that will store all books objects
+let myLibrary = [];
+
+// Book constructor
+const Book = function (title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+};
+
+// Function that adds book to the library array
+function addBookToLibrary() {
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookRead.value
+  );
+  myLibrary.push(book);
+}
+
+// ********** TEST ********** //
+
+const book1 = new Book("Atomic Habits", "James Clear", 189, true);
+const book2 = new Book("48 Laws of Power", "Robert Green", 245, false);
+const book3 = new Book(
+  "The Subtle Art of Not Giving a Fuck",
+  "Mark Manson",
+  574,
+  true
+);
+const book4 = new Book("The Laws of Human Nature", "Robert Greene", 564, false);
+
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
+
+console.log(myLibrary);
+
+// Function that adds a new row to the library table
+function addRow() {
+  tr = document.createElement("tr");
+  tdTitle = document.createElement("td");
+  tdAuthor = document.createElement("td");
+  tdPages = document.createElement("td");
+  tdRead = document.createElement("td");
+  tdRemove = document.createElement("td");
+
+  tr.appendChild(tdTitle);
+  tr.appendChild(tdAuthor);
+  tr.appendChild(tdPages);
+  tr.appendChild(tdRead);
+  tr.appendChild(tdRemove);
+
+  table.appendChild(tr);
+}
+
+function displayBooks() {
+  for (let i = 0; i <= myLibrary.length - 1; i++) {
+    addRow();
+    tdTitle.textContent = myLibrary[i].title;
+    tdAuthor.textContent = myLibrary[i].author;
+    tdPages.textContent = myLibrary[i].pages;
+    tdTitle.textContent = myLibrary[i].title;
+  }
+}
+
+displayBooks();
+
+// ******************** //
+
+doneBtn.addEventListener("click", function () {
+  addBookToLibrary();
+  closeBookModal();
+});
